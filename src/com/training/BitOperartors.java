22307eccbd775db56c8f077eb6b45bf4da3d1e14
M Rajesh <m.rajeshrajz@gmail.com>
2019-10-24 23:17:59 +0530

    added bit operators

---
+++ src/com/training/BitOperartors.java
@@ -0,0 +1,14 @@
+package com.training;
+
+public class BitOperartors {
+
+	public static void main(String[] args) {
+		int a=4,b=10;
+		System.out.println(a<<0);
+		System.out.println(a>>4);
+		System.out.println(a&1);
+		System.out.println(a|2);
+		
+	}
+
+}
