b7cbb742edcd2ccd60b4f1b7201b6f691f18d88a
M Rajesh <m.rajeshrajz@gmail.com>
2019-10-25 15:42:35 +0530

    added treemap example program

---
+++ src/com/training/collectionframework/map/TreeMapDemo.java
@@ -0,0 +1,16 @@
+package com.training.collectionframework.map;
+
+import java.util.*;
+
+public class TreeMapDemo {
+
+	public static void main(String[] args) {
+		TreeMap<String, String> map = new TreeMap<String, String>(); // sorts based on keys
+		map.put("16cs058", "Manju");
+		map.put("16cs066", "Mrudulla");
+		map.put("16cs057", "Rajesh");
+		map.put("16cs013", "Sachin");
+		System.out.println(map);
+	}
+
+}
